9ac0808aa14e412c61a5d72ae7ac66c60c70b2e9
mrbalbuena <mrbalbuena@hotmail.com>
2016-02-01 17:43:41 +0000

    Added bootstrap

---
+++ TRS/TRS/Scripts/test.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="knockout-3.4.0.debug.js" />
+/// <reference path="knockout-3.4.0.js" />
+/// <reference path="jquery-1.10.2.js" />
+
+$(function () {
+    function viewModel() {
+        var self = this;
+        self.model = {};
+        self.model.Things = ko.observableArray([
+            { ID: 1, Name: "Thing 1" },
+            { ID: 2, Name: "Thing 2" },
+            { ID: 3, Name: "Thing 3" }
+        ]);
+        self.model.CurrentDisplayThing = ko.observable(self.model.Things()[0]);
+        self.selectThing = function (item) {
+            self.model.CurrentDisplayThing(item);
+        };
+        self.doSomething = function (item) {
+            alert(item.ID);
+        }
+    }
+    ko.applyBindings(new viewModel());
+});
--- TRS/TRS/Scripts/trsApp.js
+++ TRS/TRS/Scripts/trsApp.js
@@ -4,19 +4,17 @@
 /// <reference path="jquery-1.10.2.js" />
 $(function () {
 
-    var myFunction = function () {
-        console.log('ok');
-    }
-
     var viewModel =
-    {
-        
+    {        
             trs: ko.observableArray(),
             selected: ko.observable(),
-            selectedItem: function(x) {
-                x.selected = 'true';                
+            setActive: function(item) {
+                if (!item) return;
+                console.log('setActive');
             },
-            
+            getClass: ko.computed(function(x) {
+                return 'active';
+            })
     };
 
     //var trans = [{ "TransId": 1, "TransKey": "ABOUT_DATABASECREATEDBY", "Text": "Created By", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 2, "TransKey": "ABOUT_DATABASECREATIONDATE", "Text": "Creation Date", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 3, "TransKey": "ABOUT_DATABASECREATIONMACHINE", "Text": "Created on Machine", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 4, "TransKey": "ABOUT_DATABASEDESCRIPTION", "Text": "Description", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 5, "TransKey": "ABOUT_DATABASEDETAILS", "Text": "Database Details", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 6, "TransKey": "ABOUT_DATABASEID", "Text": "Identifier", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 7, "TransKey": "ABOUT_DATABASEMASTERDATABASEID", "Text": "Master Database Id", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 8, "TransKey": "ABOUT_DOWNLOAD", "Text": "Download Version Details", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 9, "TransKey": "ABOUT_DOWNLOADVERSIONINFORMATION", "Text": "Downloads a text file containing the version information", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }, { "TransId": 10, "TransKey": "ABOUT_MIT_LICENSING", "Text": "Where an MIT license is mentioned, this is a license of the form presented here:", "Spanish": null, "BlockedBy": null, "BlockedTime": null, "TransBy": null, "TransDate": null, "CheckedBy": null, "CheckedTime": null, "RejectedBy": null, "RejectedTime": null }];
@@ -28,13 +26,7 @@ $(function () {
         
     });
 
-
-
-
-
     ko.applyBindings(viewModel);
     return viewModel;
-
-
 });
 
