3d92bfe4203c7ede54eda960842bd13c915acfc9
mrbalbuena <mrbalbuena@hotmail.com>
2016-02-15 20:45:45 +0000

    Limit results of search by word to 50 records

--- TRS/TRS/Service/TranslationService.cs
+++ TRS/TRS/Service/TranslationService.cs
@@ -78,7 +78,9 @@ namespace TRS.Service
         public IEnumerable<Translation> SearchByWord(string words)
         {
             var Translations =
-                _repo.GetAll().Where(t => t.Text.ToLower().Contains(words.ToLower()) && !string.IsNullOrEmpty(t.Spanish)).ToList();
+                _repo.GetAll().Where(t => t.Text.ToLower().Contains(words.ToLower()) && !string.IsNullOrEmpty(t.Spanish))
+                .Take(50)
+                .ToList();
             return Translations;
         }
     }
